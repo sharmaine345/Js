98efbef4720eb1ca58b96119cecf5c84dfad7d84
sharmaine345 <sharmaineannemoreno7@gmail.com>
2024-09-22 10:55:45 +0800

    Create lq1_MORENO.js

---
+++ lq1_MORENO.js
@@ -0,0 +1,44 @@
+//a persona data storage 
+# Persona 1
+persona1_name = "Maria Delos Santos Las Marias"
+persona1_age = 25
+persona1_address = "Upper Session Road, Baguio City, Benguet"
+
+# Persona 2
+persona2_name = "Juan Gamoso Dela Cruz"
+persona2_age = 28
+persona2_address = "San Nicolas, Candon City, Ilocos Sur"
+
+// b re arrange the name sequence 
+ // using split(), splitting the names
+let reArrangeper1 = persona1Name.split(" ");
+let reArrangeper2 = persona2Name.split(" ");
+def rearrange_name(full_name):
+    parts = full_name.split()
+    last_name = parts[-1]
+    first_name = parts[0]
+    middle_name = " ".join(parts[1:-1])
+    return "{last_name}, {first_name} {middle_name
+    
+ # c. Console Log Output: Print rearranged: Name, Age, Address
+("Persona 1: {persona_1['name_rearranged']}, {persona_1['age']}, {persona_1['address']}")
+("Persona 2: {persona_2['name_rearranged']}, {persona_2['age']}, {persona_2['address']}")
+
+# d. Variable Manipulations: Store the lengths of names and addresses
+length name_p1 = length(persona_1["name"])
+length name_p2 = length (persona_2["name"])
+length address_p1 = length (persona_1["address"])
+length address_p2 = length (persona_2["address"])
+
+# e. Operations
+
+# 1. Sum of all numeric values (ages, name, address)
+sum_of_all_numeric = persona_1["age"] + persona_2["age"] + length_name_persona1 + length_name_persona2 + length_address_persona1 + length_address_persona2
+
+# 2. Add the ages
+(persona_1["age"] + persona_2["age"] - length_name_persona1) * length_name_persona2
+print("Result of operation 2: {result_2}")
+
+# 3. Raise the length of Persona 1's address to the power of Persona 2's address length
+result_3 = length address_persona1 ** length address_persona2
+  
